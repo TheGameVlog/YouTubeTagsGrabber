cbb347dabce0555acb0fc7c2aa7f5b1108d0aea2
Hefin Henry Dsouza <initiotech@gmail.com>
2021-06-08 01:12:40 +0530

    -- Added Thumbnail Support -- Show Time In Readable Format on Thumbnail

--- YouTubeTagsGrabber/Form1.Designer.vb
+++ YouTubeTagsGrabber/Form1.Designer.vb
@@ -22,17 +22,24 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtYTURL = New System.Windows.Forms.TextBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtDuration = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtTags = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.prgBar = New System.Windows.Forms.ProgressBar()
+        Me.ytThumb = New System.Windows.Forms.PictureBox()
+        Me.picMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.ytThumb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.picMenu.SuspendLayout()
         Me.SuspendLayout()
         '
         'WebBrowser1
@@ -49,40 +56,40 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(13, 25)
+        Me.Label1.Location = New System.Drawing.Point(26, 25)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(105, 13)
+        Me.Label1.Size = New System.Drawing.Size(203, 13)
         Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Youtube Video URL:"
+        Me.Label1.Text = "Youtube Video URL: (Type and Hit Enter)"
         '
-        'TextBox1
+        'txtYTURL
         '
-        Me.TextBox1.Location = New System.Drawing.Point(16, 42)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(479, 20)
-        Me.TextBox1.TabIndex = 4
+        Me.txtYTURL.Location = New System.Drawing.Point(29, 42)
+        Me.txtYTURL.Name = "txtYTURL"
+        Me.txtYTURL.Size = New System.Drawing.Size(479, 20)
+        Me.txtYTURL.TabIndex = 4
         '
         'GroupBox1
         '
         Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(23, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(23, Byte), Integer))
-        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.txtDuration)
         Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.txtTags)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.ForeColor = System.Drawing.Color.White
-        Me.GroupBox1.Location = New System.Drawing.Point(16, 106)
+        Me.GroupBox1.Location = New System.Drawing.Point(29, 106)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(479, 142)
         Me.GroupBox1.TabIndex = 5
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Show Details"
         '
-        'TextBox3
+        'txtDuration
         '
-        Me.TextBox3.Location = New System.Drawing.Point(66, 109)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(85, 20)
-        Me.TextBox3.TabIndex = 7
+        Me.txtDuration.Location = New System.Drawing.Point(66, 109)
+        Me.txtDuration.Name = "txtDuration"
+        Me.txtDuration.Size = New System.Drawing.Size(85, 20)
+        Me.txtDuration.TabIndex = 7
         '
         'Label3
         '
@@ -94,13 +101,13 @@ Partial Class Form1
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Duration :"
         '
-        'TextBox2
+        'txtTags
         '
-        Me.TextBox2.Location = New System.Drawing.Point(50, 20)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(423, 75)
-        Me.TextBox2.TabIndex = 5
+        Me.txtTags.Location = New System.Drawing.Point(50, 20)
+        Me.txtTags.Multiline = True
+        Me.txtTags.Name = "txtTags"
+        Me.txtTags.Size = New System.Drawing.Size(423, 75)
+        Me.txtTags.TabIndex = 5
         '
         'Label2
         '
@@ -112,40 +119,80 @@ Partial Class Form1
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "Tags :"
         '
-        'ProgressBar1
+        'prgBar
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(16, 77)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(473, 23)
-        Me.ProgressBar1.TabIndex = 6
+        Me.prgBar.Location = New System.Drawing.Point(29, 77)
+        Me.prgBar.Name = "prgBar"
+        Me.prgBar.Size = New System.Drawing.Size(473, 23)
+        Me.prgBar.TabIndex = 6
+        '
+        'ytThumb
+        '
+        Me.ytThumb.ContextMenuStrip = Me.picMenu
+        Me.ytThumb.Location = New System.Drawing.Point(16, 270)
+        Me.ytThumb.MinimumSize = New System.Drawing.Size(512, 288)
+        Me.ytThumb.Name = "ytThumb"
+        Me.ytThumb.Size = New System.Drawing.Size(512, 288)
+        Me.ytThumb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ytThumb.TabIndex = 7
+        Me.ytThumb.TabStop = False
+        '
+        'picMenu
+        '
+        Me.picMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.picMenu.Name = "picMenu"
+        Me.picMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
+        Me.picMenu.Size = New System.Drawing.Size(181, 70)
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.ShowShortcutKeys = False
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CopyToolStripMenuItem.Text = "&Copy"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShowShortcutKeys = False
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SaveToolStripMenuItem.Text = "&Save"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(23, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(23, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(501, 257)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.ClientSize = New System.Drawing.Size(534, 569)
+        Me.Controls.Add(Me.ytThumb)
+        Me.Controls.Add(Me.prgBar)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtYTURL)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.WebBrowser1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
         Me.Name = "Form1"
         Me.Text = "Youtube Tags Grabber"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.ytThumb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.picMenu.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents WebBrowser1 As WebBrowser
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtYTURL As TextBox
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents prgBar As ProgressBar
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtTags As TextBox
+    Friend WithEvents txtDuration As TextBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents ytThumb As PictureBox
+    Friend WithEvents picMenu As ContextMenuStrip
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
 End Class
--- YouTubeTagsGrabber/Form1.vb
+++ YouTubeTagsGrabber/Form1.vb
@@ -7,7 +7,6 @@ Public Class Form1
     Dim htmlColl As HtmlElementCollection
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
     End Sub
 
 
@@ -15,33 +14,61 @@ Public Class Form1
 
     End Sub
 
-    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles txtYTURL.KeyDown
         If e.KeyData = Keys.Enter Then
-            TextBox1.Enabled = False
+            txtYTURL.Enabled = False
             WebBrowser1.ScriptErrorsSuppressed = True
-            WebBrowser1.DocumentText = New WebClient().DownloadString(TextBox1.Text)
+            WebBrowser1.DocumentText = New WebClient().DownloadString(txtYTURL.Text)
 
             htmlDOM = WebBrowser1.Document
 
         End If
     End Sub
 
+    Dim videoID, videoTitle As String
+
     Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
         htmlColl = htmlDOM.GetElementsByTagName("meta")
         For Each ele As HtmlElement In htmlColl
             If ele.Name = "keywords" Then
-                TextBox2.Text = ele.GetAttribute("content")
+                txtTags.Text = ele.GetAttribute("content")
             End If
             If ele.GetAttribute("itemprop") = "duration" Then
-                TextBox3.Text = ele.GetAttribute("content")
+                txtDuration.Text = ele.GetAttribute("content")
+            End If
+            If ele.GetAttribute("itemprop") = "videoId" Then
+                videoID = ele.GetAttribute("content")
+            End If
+            If ele.GetAttribute("itemprop") = "name" Then
+                videoTitle = ele.GetAttribute("content")
             End If
         Next
-        TextBox1.Enabled = True
+        txtYTURL.Enabled = True
+        GenerateThumbnail()
     End Sub
 
+    Public Function ConvertPTTime(ptval As String) As TimeSpan
+        Dim tempVal As String = ptval.Replace("PT", "")
+        Dim min As String = tempVal.Split("M")(0)
+        Dim sec As String = tempVal.Split("M")(1).Split("S")(0)
+
+        Dim timespan As New TimeSpan(0, min, sec)
+
+        Return timespan
+    End Function
+
+    Public Function GetReadableTime(tSpan As TimeSpan) As String
+        'removes 00: if hours is ZERO
+        If tSpan.ToString().StartsWith("00") Then
+            Return tSpan.ToString().Replace("00:", "")
+        End If
+        Return tSpan.ToString()
+    End Function
+
+
     Private Sub WebBrowser1_ProgressChanged(sender As Object, e As WebBrowserProgressChangedEventArgs) Handles WebBrowser1.ProgressChanged
-        ProgressBar1.Maximum = e.MaximumProgress
-        ProgressBar1.Value = e.CurrentProgress
+        prgBar.Maximum = e.MaximumProgress
+        prgBar.Value = e.CurrentProgress
     End Sub
 
     Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
@@ -54,5 +81,47 @@ Public Class Form1
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
         MsgBox("Click enter to start the process.", MsgBoxStyle.Information, "Information:")
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        If IsNothing(ytThumb.Image) Then
+            'Nothing to Copy
+        Else
+            Clipboard.SetImage(ytThumb.Image)
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim svDiag As New SaveFileDialog()
+        svDiag.Filter = "PNG Image (.png)|*.png|Bitmap Image (.bmp)|*.bmp|JPEG Image (.jpeg)|*.jpeg"
+        svDiag.FileName = videoID
+        If svDiag.ShowDialog = DialogResult.OK Then
+            ytThumb.Image.Save(svDiag.FileName)
+        End If
+    End Sub
+
+
+    Public Sub GenerateThumbnail()
+        Dim sF As StringFormat = New StringFormat
+        sF.LineAlignment = StringAlignment.Near
+        sF.Alignment = StringAlignment.Center
+
+        Dim OpaqueBoxBrush = New SolidBrush(Color.FromArgb(128, 0, 0, 0))
+
+
+        Dim bmp As New Bitmap(1280, 720)
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.DrawImage(Image.FromStream(WebRequest.Create("https://img.youtube.com/vi/" & videoID & "/maxresdefault.jpg").GetResponse().GetResponseStream()), New Rectangle(0, 0, bmp.Width, bmp.Height))
+            g.FillRectangle(OpaqueBoxBrush, New Rectangle(0, 640, bmp.Width, bmp.Height))
+            g.FillRectangle(OpaqueBoxBrush, New Rectangle(1040, 0, 250, 120))
+            g.DrawString(videoTitle, New Font(SystemFonts.DefaultFont.FontFamily, 26, FontStyle.Bold), Brushes.White, New Rectangle(0, 650, bmp.Width, bmp.Height), sF)
+
+            sF.LineAlignment = StringAlignment.Center
+            sF.Alignment = StringAlignment.Center
+            g.DrawString(GetReadableTime(ConvertPTTime(txtDuration.Text)), New Font(SystemFonts.DialogFont.FontFamily, 30, FontStyle.Bold), Brushes.White, New Rectangle(1040, 0, 250, 120), sF)
+        End Using
+
+        ytThumb.Image = bmp
+
     End Sub
 End Class
